33590b0c640e336bf267806722f8fb0fbf212248
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2023-08-13 18:37:34 +0300

    channel completed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,7 +3,7 @@ import { VideoCard, ChannelCard } from '../components';
 
 const Videos = ({ videos }) => {
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+    <Stack direction='row' flexWrap='wrap' justifyContent='center' gap={2}>
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
